29ca2497b0b1925e6b28b32947f8a600b4d129e1
Aritra Samanta <asamanta@purdue.edu>
2016-12-12 00:30:40 -0500

    Modified navbar

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ $(function(){
         typeSpeed: 50,
         callback: function resume_cb()
                     {
+                        document.getElementById("element").innerHTML += " |";
                         $(function() {
                            $("#resume_id").typed({
                                 strings: ["Resume"],
