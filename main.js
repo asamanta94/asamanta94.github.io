584e14b3f1e95beb59ffece697edf8d2983e557c
Aritra Samanta <asamanta@purdue.edu>
2016-12-12 00:17:42 -0500

    Added Resume

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 
 $(function(){
-    console.log("Types");
     $("#element").typed({
         strings: ["Aritra Samanta"],
-        typeSpeed: 50
+        typeSpeed: 50,
+        callback: function resume_cb()
+                    {
+                        $(function() {
+                           $("#resume_id").typed({
+                                strings: ["Resume"],
+                                typeSpeed: 50
+                            });
+                        });
+                    }
     });
 });
